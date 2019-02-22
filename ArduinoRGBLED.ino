4e5a1707e30e9e895c93e8201d8102ce3d5c0f97
Cory Siebler <csiebler@Corys-MacBook-Pro.local>
2019-02-22 00:48:13 -0700

    First commit

---
+++ ArduinoRGBLED.ino
@@ -0,0 +1,69 @@
+// Author: Cory Siebler
+// Date: 2019-02-17
+
+// Define Pins
+#define BLUE 3
+#define GREEN 5
+#define RED 6
+
+void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, LOW);
+}
+
+// define variables
+int redValue;
+int greenValue;
+int blueValue;
+
+// main loop
+void loop() {
+  // fading time between colors
+  #define delayTime 10
+
+  // choose a value between 1 and 255 to change the color.
+  redValue = 255;
+  greenValue = 0;
+  blueValue = 0;
+
+  // fades out red bring green full when i=255
+  for (int i = 0; i < 255; i++) {
+    redValue -= 1;
+    greenValue += 1;
+    analogWrite(RED, redValue);
+    analogWrite(GREEN, greenValue);
+    delay(delayTime);
+  }
+
+  redValue = 0;
+  greenValue = 255;
+  blueValue = 0;
+
+  // fades out green bring blue full when i=255
+  for (int i = 0; i < 255; i++) {
+    greenValue -= 1;
+    blueValue += 1;
+    analogWrite(GREEN, greenValue);
+    analogWrite(BLUE, blueValue);
+    delay(delayTime);
+  }
+
+  redValue = 0;
+  greenValue = 0;
+  blueValue = 255;
+
+  // fades out blue bring red full when i=255
+  for (int i = 0; i < 255; i++) {
+    // The following code has been rearranged to match the other two similar sections
+    blueValue -= 1;
+    redValue += 1;
+
+    analogWrite(BLUE, blueValue);
+    analogWrite(RED, redValue);
+    delay(delayTime);
+  }
+}
